1762c54b6f9d7094b095ea1ed806f66975c3a557
Thomas Owens <thomasowens2000@gmail.com>
2019-11-17 11:23:33 -0500

    Added graphics and scenario input.

--- src/Body.java
+++ src/Body.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.security.InvalidParameterException;
 import java.util.Arrays;
 
@@ -6,7 +7,7 @@ public class Body {
     private int[] position;
     private int[] velocity;
 
-    public Body(int mass, int[] position, int[] velocity) throws InvalidParameterException {
+    Body(int mass, int[] position, int[] velocity) throws InvalidParameterException {
         if (mass < 0 || position.length != 2 || position[0] < 0 || position[1] < 0) {
             throw new InvalidParameterException();
         }
@@ -20,7 +21,7 @@ public class Body {
      * Updates the velocity and position of this Body given net force in x and y directions.
      * @param force the net force on this Body
      */
-    public void update(int[] force) {
+    void update(int[] force) {
         Arrays.setAll(velocity, i -> velocity[i] + force[i]/mass);
         Arrays.setAll(position, i -> position[i] + velocity[i]);
     }
@@ -31,5 +32,16 @@ public class Body {
 
     public int[] getPosition() {
         return position;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + mass + ", " + Arrays.toString(position) + ", " + Arrays.toString(velocity) + "}";
+    }
+
+    public void paint(Graphics2D g2d) {
+        int size = (int) Math.ceil(Math.sqrt(mass) * 10);
+        g2d.setColor(Color.BLACK);
+        g2d.fillOval(position[0] - size/2, position[1] - size/2, size, size);
     }
 }
--- src/NBodyDriver.java
+++ src/NBodyDriver.java
@@ -1,3 +1,125 @@
-public class NBodyDriver {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NBodyDriver extends JPanel {
+
+    private Body[] bodies;
+
+    private NBodyDriver(String filepath) throws FileNotFoundException {
+        File scenario = new File(filepath);
+        Scanner s = new Scanner(scenario);
+        bodies = new Body[Integer.parseInt(s.nextLine().strip())];
+        int i = 0;
+        while (s.hasNextLine()) {
+            int[] line = Arrays.stream(s.nextLine().strip().split(",")).mapToInt(Integer::parseInt).toArray();
+            bodies[i] = new Body(line[0], new int[]{line[1], line[2]}, new int[]{line[3], line[4]});
+            i++;
+        }
+    }
+
+    /**
+     * Updates the simulation.
+     */
+    private void update() {
+
+    }
+
+    /**
+     * Renders the simulation using Graphics2D.
+     * @param graphics a Graphics instance
+     */
+    @Override
+    public void paintComponent(Graphics graphics) {
+        Graphics2D g2d = (Graphics2D) graphics;
+        for (Body b : bodies) {
+            b.paint(g2d);
+        }
+    }
+
+    /**
+     * Renders a String in the window using Graphics2D.
+     * @param g a Graphics2D instance
+     * @param text the String to render
+     * @param size the font size
+     * @param outline the width of the outline
+     * @param x the center x of the text
+     * @param y the center y of the text
+     */
+    public static void renderString(Graphics2D g, String text, int size, int outline, int x, int y) {
+
+        Graphics2D g2d = (Graphics2D) g.create();
+
+        Font f = new Font("Cambria", Font.PLAIN, size);
+        RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHints(rh);
+        g2d.setFont(f);
+
+        FontRenderContext frc = g2d.getFontRenderContext();
+        TextLayout tl = new TextLayout(text, g2d.getFont(), frc);
+
+        int descent = (int) tl.getDescent();
+        int ascent = (int) tl.getAscent();
+
+        int center_x = x - (int) tl.getBounds().getCenterX();
+        int center_y = y - (2 * descent - ascent) / 2;
+
+        AffineTransform at = new AffineTransform();
+        at.translate(center_x, center_y);
+        Shape text_shape = tl.getOutline(null);
+
+        g2d.transform(at);
+        g2d.setStroke(new BasicStroke(outline, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        g2d.setColor(Color.WHITE);
+        g2d.draw(text_shape);
+
+        g2d.setColor(Color.BLACK);
+        g2d.fill(text_shape);
+
+        g2d.dispose();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // Create JFrame, Game, add Game to JFrame and set settings
+        JFrame frame = new JFrame("N-Body Simulation");
+
+        NBodyDriver nbd = new NBodyDriver("data/testingscenario.txt");
+        nbd.setPreferredSize(new Dimension(600, 600));
+
+        frame.add(nbd);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create update and repaint timer and start it.
+        Timer t = new Timer(10, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                nbd.update();
+                nbd.repaint();
+            }
+        });
+
+        t.start();
+
+        // Create WindowListener to stop timer on close, ending the process.
+        frame.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                t.stop();
+            }
+        });
+    }
 
 }
